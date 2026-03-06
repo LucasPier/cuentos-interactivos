deeb44c99b4dd7a1fda7332ac5e012b5cc2c1e20
LucasPier <lucascardoni@gmail.com>
2026-03-06 17:19:13 -0300

    chore(sw): actualizar cache-biblioteca a v6 y app a 1.2.2

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 'use strict';
 (() => {
     // Versión general de la aplicación 
-    const VERSION_APP = "1.2.1";
+    const VERSION_APP = "1.2.2";
 
     // Versiones de caché
-    const CACHE_BIBLIOTECA = '5',
+    const CACHE_BIBLIOTECA = '6',
         CACHE_CSS = '10',
         CACHE_JS = '10',
         CACHE_CHALLENGES = '4',
