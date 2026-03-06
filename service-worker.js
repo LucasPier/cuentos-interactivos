b5045f88d1a993951e3d282514e0187a18c5ae8a
LucasPier <lucascardoni@gmail.com>
2026-03-06 18:53:36 -0300

    fix(pwa): ajustar scope, start_url y modo standalone
    Se configuró el scope y start_url con rutas absolutas para el subdominio de GitHub Pages para forzar la correcta generación del WebAPK. Se cambió display a standalone con override fullscreen.

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 'use strict';
 (() => {
     // Versión general de la aplicación 
-    const VERSION_APP = "1.2.4";
+    const VERSION_APP = "1.2.5";
 
     // Versiones de caché
-    const CACHE_BIBLIOTECA = '8',
+    const CACHE_BIBLIOTECA = '9',
         CACHE_CSS = '10',
         CACHE_JS = '10',
         CACHE_CHALLENGES = '4',
